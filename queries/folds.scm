da6d85db0e66f3b76d70ff5c2b119803ae206d02
Himujjal <himu@tuta.io>
2021-03-23 02:22:45 +0530

    added queries & fixed js indentation - prettier

---
+++ queries/folds.scm
@@ -0,0 +1,15 @@
+[
+  (block)
+  (comptime_block)
+  (defer_block)
+  (suspend_block)
+  (resume_block)
+  (if_expression)
+  (switch_block)
+  (for_expression)
+  (test_expressio)
+  (struct_expression)
+  (anonymous_struct_enum)
+  (union_expression)
+  (enum_expression)
+] @fold
